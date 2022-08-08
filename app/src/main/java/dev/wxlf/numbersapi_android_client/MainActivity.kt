5d340aacd6490da89491efb09df921d50c505aee
wxlf512 <wxlf512@gmail.com>
2022-08-08 22:41:35 +0300

    Initial commit

---
+++ app/src/main/java/dev/wxlf/numbersapi_android_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.wxlf.numbersapi_android_client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
