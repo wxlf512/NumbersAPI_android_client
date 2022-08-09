901e263319dcb8ede5024e6a646fb9cb2212d573
wxlf512 <wxlf512@gmail.com>
2022-08-09 13:20:03 +0300

    Added test UI

--- app/src/main/java/dev/wxlf/numbersapi_android_client/MainActivity.kt
+++ app/src/main/java/dev/wxlf/numbersapi_android_client/MainActivity.kt
@@ -2,6 +2,8 @@ package dev.wxlf.numbersapi_android_client
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
@@ -14,9 +16,13 @@ class MainActivity : MvpAppCompatActivity(), MainView{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.showFactBtn).setOnClickListener {
+            mainPresenter.fetchFact(findViewById<EditText>(R.id.userNum).text.toString().toInt())
+        }
     }
 
     override fun writeFact(fact: String) {
-        TODO("Not yet implemented")
+        findViewById<TextView>(R.id.factView).text = fact
     }
 }
--- app/src/main/java/dev/wxlf/numbersapi_android_client/MainPresenter.kt
+++ app/src/main/java/dev/wxlf/numbersapi_android_client/MainPresenter.kt
@@ -5,5 +5,9 @@ import moxy.MvpPresenter
 
 @InjectViewState
 class MainPresenter: MvpPresenter<MainView>() {
+    private val TAG: String = MainPresenter::class.java.simpleName
 
+    fun fetchFact(num: Int) {
+        viewState.writeFact("${num}")
+    }
 }
