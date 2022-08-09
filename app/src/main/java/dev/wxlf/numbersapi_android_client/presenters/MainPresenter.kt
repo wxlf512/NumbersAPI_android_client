2a391a538010f86867c055bcd8fd51cb3e439a44
wxlf512 <wxlf512@gmail.com>
2022-08-09 21:14:12 +0300

    Fix date parsing

--- app/src/main/java/dev/wxlf/numbersapi_android_client/presenters/MainPresenter.kt
+++ app/src/main/java/dev/wxlf/numbersapi_android_client/presenters/MainPresenter.kt
@@ -1,7 +1,5 @@
 package dev.wxlf.numbersapi_android_client.presenters
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import dev.wxlf.numbersapi_android_client.remote.NumbersApi
 import dev.wxlf.numbersapi_android_client.views.MainView
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -9,11 +7,9 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import moxy.InjectViewState
 import moxy.MvpPresenter
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @InjectViewState
-class MainPresenter: MvpPresenter<MainView>() {
+class MainPresenter : MvpPresenter<MainView>() {
 
     private val TAG: String = MainPresenter::class.java.simpleName
     private val compositeDisposable = CompositeDisposable()
@@ -26,26 +22,27 @@ class MainPresenter: MvpPresenter<MainView>() {
 
     fun fetchFact(num: String, type: Int, numbersApi: NumbersApi) {
         if (type == 3 && num != "random") {
-            val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd")
-            val date = LocalDate.parse("2022/$num", formatter).atStartOfDay()
-            compositeDisposable.add(numbersApi.getDateFact(date.month.value, date.dayOfMonth)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    viewState.writeFact(it)
-                }, {
-                    viewState.showError(it.localizedMessage as String)
-                })
+            val date = num.split("/")
+            compositeDisposable.add(
+                numbersApi.getDateFact(date[0].toInt(), date[1].toInt())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        viewState.writeFact(it)
+                    }, {
+                        viewState.showError(it.localizedMessage as String)
+                    })
             )
         } else {
-            compositeDisposable.add(numbersApi.getFact(num, typesList[type])
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    viewState.writeFact(it)
-                }, {
-                    viewState.showError(it.localizedMessage as String)
-                })
+            compositeDisposable.add(
+                numbersApi.getFact(num, typesList[type])
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        viewState.writeFact(it)
+                    }, {
+                        viewState.showError(it.localizedMessage as String)
+                    })
             )
         }
     }
